a8b79bf3e0da20e8c3561dc8c2ce3517689ee877
Your Name <you@example.com>
2018-08-13 19:59:53 +1200

    first commit

---
+++ Sample Application/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample_Application
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Open chrome browser  
+            IWebDriver driver = new Chromedriver(@"C:\Users\SONY\Downloads\chromedriver_win32");
+
+            //Go to the url
+            driver.Navigate().GoToUrl("http://horse-dev.azurewebsites.net/Account/Login?ReturnUrl=%2f");
+        }
+    }
+}
---
+++ Sample Application/WebDriver.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sample_Application
+{
+    class WebDriver
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    {
+    }
+}
